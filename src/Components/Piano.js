5586e6768cdf143253e9e97891a6edca392ce05b
Tara <taradavies@gmail.com>
2021-01-30 13:27:01 -0500

    initial commit includes piano, constants, keys, css

--- src/Components/Piano.js
+++ src/Components/Piano.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import _ from 'lodash';
+import { Key } from './Key.js';
+import '../Piano.css';
+import { Notes } from '../global/constants.js'
  
 class Piano extends React.Component {
     render () {
+
+        const keys = _.map(Notes, (note, index) => {
+            return (
+                <Key
+                key={index}
+                note={note}
+                />
+            );
+        });
+        
         return (
-            <div>
-                <h1>
-                    Piano!
-                </h1>
+            <div className="piano">
+               {keys}
             </div>
-        )
+        );
     }
 }
 
