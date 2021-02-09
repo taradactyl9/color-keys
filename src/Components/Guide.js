fb325428f5e5774365dd0d6606bf8d34bbb373cf
Tara <taradavies@gmail.com>
2021-02-09 00:32:42 -0500

    added key legend

---
+++ src/Components/Guide.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Guide extends React.Component {
+    render () {
+        return (
+        <div className="guide">
+            <h3>Key Legend
+               <ul>z: c </ul> 
+               <ul>s: df</ul>
+               <ul>x: d</ul>
+               <ul>d: ef</ul>
+               <ul>c: e</ul> 
+               <ul>v: f</ul>
+               <ul>g: gf</ul>
+               <ul>b: g</ul>
+               <ul>h: af</ul>
+               <ul>n: a</ul>
+               <ul>j: bf</ul>
+               <ul>m: b</ul>
+            </h3>                               
+        </div> 
+        );
+    }
+}
+
+export { Guide };
